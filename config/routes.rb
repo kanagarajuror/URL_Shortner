c73b7ccc95e6f524c7b062feed789476e616a87c
kanagaraju <kanagarajuror@gmail.com>
2019-11-16 17:16:34 +0530

    first commit for url shortner

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to:'shortended_urls#index'
+get "/:short_url", to: "shortended_urls#show"
+get "shortended/:short_url",  to: "shortended_urls#shortended", as: :shortended
+post "/shortended_urls/create"
+get "/shortended_urls/fetch_original_url"
+end
